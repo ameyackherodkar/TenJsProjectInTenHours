b6d785fab34142ff5f090ea5db64d86ca37b6c7e
Ameya Kherodkar <ameyack@gmail.com>
2021-01-10 00:36:43 -0500

    2nd commit

--- receipe-app/script.js
+++ receipe-app/script.js
@@ -1,22 +1,22 @@
-function list(names){
-    var output='';
-    for(var i = 0 ; i <names.length;i++){
-      if(names.length == 1)
-       output = names[0].name.toString();
-      else
-      {
-          if(i != names.length-1){
-            output +=names[i].name.toString() + ',';
-          }else{
-            output = output.slice(0,output.length-1);
-            console.log(output);
-            output += ' & '+names[i].name.toString();
-            console.log(output);
+// function list(names){
+//     var output='';
+//     for(var i = 0 ; i <names.length;i++){
+//       if(names.length == 1)
+//        output = names[0].name.toString();
+//       else
+//       {
+//           if(i != names.length-1){
+//             output +=names[i].name.toString() + ',';
+//           }else{
+//             output = output.slice(0,output.length-1);
+//             console.log(output);
+//             output += ' & '+names[i].name.toString();
+//             console.log(output);
             
-          }
-      }
-    }
-    return output;
-  }
+//           }
+//       }
+//     }
+//     return output;
+//   }
 
-  list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'},{name: 'Homer'},{name: 'Marge'} ]);
+//   list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'},{name: 'Homer'},{name: 'Marge'} ]);
